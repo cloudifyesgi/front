176c9165486effcb22e10e86d68356976b24c560
Zineddine Boudaoud <zinedddine97@gmail.com>
2019-05-18 19:42:47 +0200

    initial commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,54 @@
+export const navItems = [
+  {
+    name: 'Personal cloud',
+    url: '/dashboard',
+    icon: 'cui-cloud',
+    children: [
+      {
+        name: 'Favorite 1',
+        url: './',
+        icon: 'cui-star'
+      },
+      {
+        name: 'Favorite 2',
+        url: './',
+        icon: 'cui-star'
+      },
+      {
+        name: 'Favorite 3',
+        url: './',
+        icon: 'cui-star'
+      }
+    ]
+  },
+  {
+    name: 'Shared clouds',
+    url: '/dashboard',
+    icon: 'cui-share'
+  },
+  {
+    name: 'Recently updated',
+    url: '/dashboard',
+    icon: 'cui-cloud-upload'
+  },
+  {
+    name: 'Followed file',
+    url: '/dashboard',
+    icon : 'cui-file'
+  },
+  {
+    name: 'Synchronized computer',
+    url: '/dashboard',
+    icon: 'cui-laptop'
+  },
+  {
+    name: 'Bin',
+    url: '/dashboard',
+    icon: 'cui-trash'
+  },
+  {
+    name: 'My user groups',
+    url: '/dashboard',
+    icon: 'cui-user'
+  }
+];
